0c1a3ef7d01004336687815caf380705e553e2b4
Provakar Datta <pdbforce@ifarm1801.jlab.org>
2022-09-09 12:14:41 -0400

    Added the run and launch scripts to submit sbsdig jobs to the farm

---
+++ run-sbsdig.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --partition=production
+#SBATCH --account=halla
+#SBATCH --mem-per-cpu=1500
+
+#SWIF_JOB_WORK_DIR=$PWD # for testing purposes
+echo 'swif_job_work_dir='$SWIF_JOB_WORK_DIR
+
+MODULES=/etc/profile.d/modules.sh 
+
+if [[ $(type -t module) != function && -r ${MODULES} ]]; then 
+source ${MODULES} 
+fi 
+
+if [ -d /apps/modulefiles ]; then 
+module use /apps/modulefiles 
+fi 
+
+# setup farm environments
+module load gcc/9.2.0 
+source /site/12gev_phys/softenv.sh 2.4
+ldd /work/halla/sbs/pdbforce/LIBSBSDIG/install/bin/sbsdig |& grep not
+
+# Setup sbsdig specific environments
+export LIBSBSDIG=/work/halla/sbs/pdbforce/LIBSBSDIG/install
+source $LIBSBSDIG/bin/sbsdigenv.sh
+
+# run the sbsdig command
+dbfile=$LIBSBSDIG/db/db_gmn_conf.dat
+
+txtfile=$1 # .txt file containing input file paths
+infilename=$2
+
+echo $infilename >>$txtfile
+
+sbsdig $dbfile $txtfile
+
---
+++ submit-sbsdig-job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+inputfile=$1 # .txt file containing input file paths
+jobname=$2
+outdirpath='/lustre19/expphy/volatile/halla/sbs/pdbforce/g4sbs_output'
+
+script='/work/halla/sbs/pdbforce/jlab-farm-replay/run-sbsdig.sh'
+
+swif2 add-job -workflow test-g4sbs -partition production -name $jobname -cores 1 -disk 5GB -ram 1500MB $script $inputfile $outdirpath
+
+#$script $inputfile $outdirpath # for testing purposes
