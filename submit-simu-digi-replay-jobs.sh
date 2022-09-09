5084f22cc79821e7d60f1b1913a1ba9caa961997
Provakar Datta <pdbforce@ifarm1801.jlab.org>
2022-09-09 12:15:55 -0400

    Added script to submit g4sbs, sbsdig, and replay jobs together such that they run in proper sequence

---
+++ submit-simu-digi-replay-jobs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+preinit=$1 # don't add file extension
+nevents=$2
+njobs=$3
+workflowname='test3-g4sbs'
+# specify a directory on volatile to store g4sbs, sbsdig, & replayed files.
+# Working on a single directory is convenient safe for the above mentioned
+# three processes to run smoothly.
+outdirpath='/lustre19/expphy/volatile/halla/sbs/pdbforce/g4sbs_output'
+
+# Validating the number of arguments provided
+if [[ "$#" -ne 3 ]]; then
+    echo -e "\n--!--\n Illegal number of arguments!!"
+    echo -e " This script expects 3 arguments: <preinit> <nevents> <njobs> \n"
+    exit;
+fi
+
+for ((i=0; i<=$njobs; i++))
+do
+    # lets submit g4sbs jobs first
+    outfilename=$preinit'_job_'$i'.root'
+    postscript=$preinit'_job_'$i'.mac'
+    g4sbsjobname=$preinit'_job_'$i
+
+    g4sbsscript='/work/halla/sbs/pdbforce/jlab-farm-replay/run-g4sbs-simu.sh'
+
+    swif2 add-job -workflow $workflowname -partition production -name $g4sbsjobname -cores 1 -disk 5GB -ram 1500MB $g4sbsscript $preinit $postscript $nevents $outfilename $outdirpath
+
+    # now, it's time for digitization
+    txtfile=$preinit'_job_'$i'.txt'
+    sbsdigjobname=$preinit'_digi_job_'$i
+    sbsdiginfile=$outdirpath'/'$outfilename
+
+    sbsdigscript='/work/halla/sbs/pdbforce/jlab-farm-replay/run-sbsdig.sh'
+    
+    swif2 add-job -workflow $workflowname -antecedent $g4sbsjobname -partition production -name $sbsdigjobname -cores 1 -disk 5GB -ram 1500MB $sbsdigscript $txtfile $sbsdiginfile
+
+    # finally, lets replay the digitized data
+    digireplayinfile=$preinit'_job_'$i
+    digireplayjobname=$preinit'_digi_replay_job_'$i
+
+    digireplayscript='/work/halla/sbs/pdbforce/jlab-farm-replay/run-digi-replay.sh'
+    
+    swif2 add-job -workflow $workflowname -antecedent $sbsdigjobname -partition production -name $digireplayjobname -cores 1 -disk 5GB -ram 1500MB $digireplayscript $digireplayinfile $outdirpath
+done
