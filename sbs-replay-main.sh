4737246bfbcc2e71fdaf1b6a745db994ba7c7084
Sean Jeffas <jeffas@ifarm1901.jlab.org>
2023-07-25 12:00:37 -0400

    Added functionality for various types of replays using the same scripts

---
+++ sbs-replay-main.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------- #
+# This script submits real data replay jobs for GMn/nTPE data to batch farm.# 
+# There is a flag to force all the jobs to run on ifarm instead. It was     #
+# created based on Andrew Puckett's script.                                 #
+# ---------                                                                 #
+# Sean Jeffas, sj9ry@virginia.edu CREATED 07-24-2022                        #
+# ---------                                                                 #
+# ** Do not tamper with this sticker! Log any updates to the script above.  #
+# ------------------------------------------------------------------------- #
+
+# Setting necessary environments (ONLY User Specific part)
+export SCRIPT_DIR=/w/halla-scshelf2102/sbs/jeffas/jlab-HPC
+export ANALYZER=/w/halla-scshelf2102/sbs/jeffas/ANALYZER/install
+export SBSOFFLINE=/w/halla-scshelf2102/sbs/jeffas/SBS_OFFLINE/install
+export SBS_REPLAY=/w/halla-scshelf2102/sbs/jeffas/SBS_OFFLINE/SBS-replay
+export GMN_DATA_PATH=halla/sbs/raw
+export GEN_DATA_PATH=halla/sbs/GEnII/raw
+
+runs=$1       # run number 
+prefix=-1     # We will initialize the rest in a second
+run_on_ifarm=-1
+nevents=-1
+maxsegments=-1
+segments_per_job=-1
+use_sbs_gems=             # 0 = no sbs gems, 1 = use sbs gems
+# Workflow name (Not relevant if run_on_ifarm = 1)
+workflowname=jeffas_test
+# Specify a directory on volatile to store replayed ROOT files
+outdirpath=/lustre19/expphy/volatile/halla/sbs/jeffas/GEN_root/Rootfiles
+
+type=0  # 1 = multi run from txt file, 0 = single run
+
+#Description of how to run if the user puts in a wrong input
+if [ "$#" -ne 6 ] && [ "$#" -ne 3 ] && [ "$#" -ne 7 ]; then
+    echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo -e "This script expects 2 options for inputs:\n"
+    echo -e "Option 1: sbs-replay-main.sh <runnum> <prefix> <nevents> <maxsegments> <segments_per_job> <run_on_ifarm> <use_sbs_gems (optional)>"
+    echo -e "<use_sbs_gems> is optional (off by default)\n"
+    echo -e "or\n"
+    echo -e "Option 2: sbs-replay-main.sh <runlist> <maxsegments> <segments_per_job> <use_sbs_gems (optional)>"
+    echo -e "<use_sbs_gems> is optional (off by default)\n"
+    echo -e "For option 2 the runlist text file must have the following form:"
+    echo -e "<prefix>"
+    echo -e "<output_dir_name>"
+    echo -e "<workflow_name>"
+    echo -e "runnum1"
+    echo -e "runnum2"
+    echo -e "runnum3"
+    echo -e "."
+    echo -e "."
+    echo -e "."
+    echo -e " "
+    exit;
+fi
+
+#This reads the input and tells if it is a text file or a integer
+#If an integer we assume it's a single run, if text file then a list of runs
+re='^[0-9]+$'
+if ! [[ $runs =~ $re ]] ; then
+    type=1;
+fi
+
+
+# If this is a single run replay then we do that
+if [ $type -eq 0 ]; then
+    if [ "$#" -ne 6 ] && [ "$#" -ne 7 ]; then
+	echo -e "!!!! Error, single run replay needs 6 or 7 arguments !!!!"
+	echo -e "sbs-replay-main.sh <runnum> <prefix> <nevents> <maxsegments> <segments_per_job> <run_on_ifarm> <use_sbs_gems (optional)>\n"
+	exit
+    fi
+    #read in variables expected for a single run replay
+    prefix=$2
+    nevents=$3
+    maxsegments=$4 
+    segments_per_job=$5 
+    run_on_ifarm=$6
+    use_sbs_gems=$7
+
+    #if use_sbs_gems has no input assume it is 0 (not used)
+    if [ -z "$use_sbs_gems" ]
+    then
+	use_sbs_gems=0
+    fi
+    #If this is a GMN run force sbs gems to be off
+    if [ $prefix = 'e1209019' ]
+    then
+	use_sbs_gems=0
+    fi
+
+elif [ $type -eq 1 ]; then   #Otherwise do a runlist replay
+    if [ "$#" -ne 3 ]; then
+	echo -e "!!!! Error, runlist replay needs 3 arguments !!!!"
+	echo -e "sbs-replay-main.sh <runlist> <maxsegments> <segments_per_job> <use_sbs_gems (optional)>\n"
+	exit
+    fi
+    nevents=-1
+    maxsegments=2 
+    segments_per_job=3 
+    run_on_ifarm=0
+
+    #read the configuration info from the text file
+    if [ ! -f "$runs" ]; then
+	echo "!!!! Error, $runs does not exist !!!!"
+	exit
+    fi
+
+    prefix=$(head -n 1 "$runs" | tail -n 1)
+    outdirpath=$(head -n 2 "$runs" | tail -n 1)
+    workflowname=$(head -n 3 "$runs" | tail -n 1)
+fi
+
+
+#Automatically set the data path pased on the experiment number
+#Will need to get updated as more experiments are added
+export DATA_PATH=halla/sbs/GEnII/raw 
+
+if [ $prefix = 'e1209019' ]
+then
+    export DATA_PATH=$GMN_DATA_PATH
+fi
+if [ $prefix = 'e1209016' ]
+then
+    export DATA_PATH=$GEN_DATA_PATH
+fi
+
+
+#if use_sbs_gems has no input assume it is 0 (not used)
+    if [ -z "$use_sbs_gems" ]
+    then
+	use_sbs_gems=0
+    fi
+
+#Check the workflow and out directory name
+if [[ $run_on_ifarm -ne 1 ]]; then
+    echo -e ' "workflowname" : '$workflowname''
+fi
+echo -e ' "outdirpath"   : '$outdirpath' \n------'
+while true; do
+    read -p "Do they look good? [y/n] " yn
+    echo -e ""
+    case $yn in
+	[Yy]*) 
+	    break; ;;
+	    [Nn]*) 
+	    if [[ $run_on_ifarm -ne 1 ]]; then
+		read -p "Enter desired workflowname : " temp1
+		workflowname=$temp1
+	    fi
+	    read -p "Enter desired outdirpath   : " temp2
+	    outdirpath=$temp2		
+	    break; ;;
+    esac
+done
+
+
+# Create the output directory if necessary
+if [[ ! -d $outdirpath ]]; then
+    { #try
+	mkdir $outdirpath
+    } || { #catch
+	echo -e "\n!!!!!!!! ERROR !!!!!!!!!"
+	echo -e $outdirpath "doesn't exist and cannot be created! \n"
+	exit;
+    }
+fi
+if [[ ! -d $outdirpath'/rootfiles' ]]; then
+    { #try
+	mkdir $outdirpath'/rootfiles'
+    } || { #catch
+	echo -e "\n!!!!!!!! ERROR !!!!!!!!!"
+	echo -e $outdirpath'/rootfiles' "doesn't exist and cannot be created! \n"
+	exit;
+    }
+fi
+if [[ ! -d $outdirpath'/logs' ]]; then
+    { #try
+	mkdir $outdirpath'/logs'
+    } || { #catch
+	echo -e "\n!!!!!!!! ERROR !!!!!!!!!"
+	echo -e $outdirpath'/logs' "doesn't exist and cannot be created! \n"
+	exit;
+    }
+fi
+
+
+
+# Creating the workflow
+if [[ $run_on_ifarm -ne 1 ]]; then
+    swif2 create $workflowname
+else
+    echo -e "\nRunning all jobs on ifarm!\n"
+fi
+
+#if a single run then we do a single job
+if [ $type -eq 0 ]; then
+    submit-sbs-jobs.sh $runs $prefix $nevents $maxsegments $segments_per_job $use_sbs_gems $run_on_ifarm $outdirpath $workflowname $SCRIPT_DIR $ANALYZER $SBSOFFLINE $SBS_REPLAY $DATA_PATH
+fi
+
+
+line_num=1
+#if a runlist then we loop over all the runs in the file
+if [ $type -eq 1 ]; then
+    while read runs;
+    do 
+	#skip the first few lines which were read earlier to get the configuration
+	if (( line_num < 4 ));then
+	    line_num=$((line_num + 1))
+	    continue
+	fi
+	submit-sbs-jobs.sh $runs $prefix $nevents $maxsegments $segments_per_job $use_sbs_gems $run_on_ifarm $outdirpath $workflowname $SCRIPT_DIR $ANALYZER $SBSOFFLINE $SBS_REPLAY $DATA_PATH
+    done < $runs
+fi
+
+if [[ $run_on_ifarm -ne 1 ]]; then
+    swif2 run $workflowname
+    swif2 status $workflowname
+fi
---
+++ submit-sbs-jobs.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------- #
+# This script submits real data replay jobs for GMn/nTPE data to batch farm.# 
+# There is a flag to force all the jobs to run on ifarm instead. It was     #
+# created based on Andrew Puckett's script.                                 #
+# ---------                                                                 #
+# P. Datta <pdbforce@jlab.org> CREATED 11-09-2022                           #
+# ---------                                                                 #
+# ** Do not tamper with this sticker! Log any updates to the script above.  #
+# ------------------------------------------------------------------------- #
+
+runnum=$1
+prefix=$2
+nevents=$3
+maxsegments=$4
+segments_per_job=$5
+use_sbs_gems=$6
+run_on_ifarm=$7
+outdirpath=$8
+workflowname=$9
+scriptdir=${10}
+analyzerenv=${11}
+sbsofflineenv=${12}
+sbsreplayenv=${13}
+datapath=${14}
+
+#Set environments from inputs above
+export SCRIPT_DIR=$scriptdir
+export ANALYZER=$analyzerenv
+export SBSOFFLINE=$sbsofflineenv
+export SBS_REPLAY=$sbsreplayenv
+export DATA_PATH=$datapath
+export DATA_DIR=/cache/$DATA_PATH
+
+firstsegment=0
+lastsegment=0
+nsegments=0
+
+# look for first segment of first stream on cache disk:
+firstsegname=$prefix'_'$runnum'.evio.0.0'
+mssfirst='mss:/mss/'$DATA_PATH/$firstsegname
+cachefirst='/cache/mss/'$DATA_PATH/$firstsegname
+
+script=$SCRIPT_DIR'/run-sbs-replay.sh'
+
+# no matter what we require the first stream, first segment for each job (must exist)
+# so we initialize input string to the first segment of first stream:
+inputstring=' -input '$cachefirst' '$mssfirst' '
+
+for ((i=0; i<=$maxsegments; i++))
+do
+
+    segment_i_added=0 # this will be set to 1 if at least segment i stream 0 is found:
+    
+    if(( $nsegments == 0 )); then # we haven't added any segments to this job yet:
+	# initialize firstsegment and inputstring:
+	firstsegment=$i
+	inputstring=' -input '$cachefirst' '$mssfirst' '
+    fi
+
+    # look for streams zero through 2 for segment i: $i is segment number $j is stream number
+    for((j=0; j<=2; j++))
+    do 
+	eviofilename=$prefix'_'$runnum'.evio.'$j'.'$i
+	mssfilename='mss:/mss/'$DATA_PATH/$eviofilename
+	cachefile='/cache/mss/'$DATA_PATH/$eviofilenam
+
+	testfilename='/mss/'$DATA_PATH/$eviofilename
+	
+	if [ -f "$testfilename" ]; 
+	then
+	    if(( $j == 0 )); then
+		(( nsegments++ ))
+		segment_i_added=1
+	    fi
+	    if(( !($j == 0 && $i == 0) )); then # unless this is the first segment of stream 0, add the file to the job:
+		inputstring+=' -input '$cachefile' '$mssfilename' '
+	    fi
+	fi
+    done # end loop on streams 0-2
+    
+    # After adding any files found from this segment number to the job,
+    # check if this is the last segment or if adding this segment caused us to reach the target number of segments per job:
+    if(( $segment_i_added == 1 )); then # if we at least found stream 0 for this segment, proceed: 
+	# increment the number of segments:
+	# NO! This was already done above! ((nsegments++))
+	if (( $i == $maxsegments || $nsegments == $segments_per_job )); 
+	    then # this is either the last segment or we reached the required number of segments per job. 
+	    # Go ahead and launch the job and then reset segment counter:
+	    lastsegment=$i 
+	    jobname=$prefix'_replay_'$runnum'_segments'$firstsegment'_'$lastsegment
+	    
+	    echo 'Submitting job '$jobname' with '$nsegments' segments, runnum='$runnum
+	    #		echo 'Input string = '$inputstring
+	    
+	    scriptrun=$script' '$runnum' '$nevents' 0 '$prefix' '$firstsegment' '$nsegments' '$use_sbs_gems' '$DATA_PATH' '$outdirpath' '$run_on_ifarm' '$ANALYZER' '$SBSOFFLINE' '$SBS_REPLAY
+	    addjobcmd='add-job -workflow '$workflowname' -partition production -name '$jobname' -cores 1 -disk 25 GB -ram 3000 MB '$inputstring' '$scriptrun
+	    
+	    if [[ $run_on_ifarm -ne 1 ]]; then
+	        swif2 $addjobcmd
+	    else
+		$scriptrun
+	    fi
+
+       	    nsegments=0
+	fi   
+    else # current segment NOT added; if any segments have been added to the job, go ahead and submit the job if applicable
+	if (( $nsegments > 0 ))
+	then
+	    ((lastsegment=$i-1))
+	    jobname=$prefix'_replay_'$runnum'_segments'$firstsegment'_'$lastsegment
+	    
+	    echo 'Submitting job '$jobname' with '$nsegments' segments, runnum = '$runnum
+#	    echo 'Input string = "'$inputstring'"'
+
+	    scriptrun=$script' '$runnum' '$nevents' 0 '$prefix' '$firstsegment' '$nsegments' '$use_sbs_gems' '$DATA_PATH' '$outdirpath' '$run_on_ifarm' '$ANALYZER' '$SBSOFFLINE' '$SBS_REPLAY' '$usesbsgems
+	    addjobcmd='add-job -workflow '$workflowname' -partition production -name '$jobname' -cores 1 -disk 25 GB -ram 3000 MB '$inputstring' '$scriptrun
+
+	    if [[ $run_on_ifarm -ne 1 ]]; then
+	        swif2 $addjobcmd
+	    else
+		$scriptrun
+	    fi
+
+	    nsegments=0
+	fi
+	break
+    fi
+done
